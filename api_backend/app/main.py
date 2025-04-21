4895a2039ac931def7567c9a78a7005e6cf32ffb
Udasco, Dou Issa Steffi <160296940+steffidou@users.noreply.github.com>
2025-04-22 01:17:55 +0800

    backend changes

--- api_backend/app/main.py
+++ api_backend/app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from app.database import SessionLocal, engine, Base
 from app import crud, schemas
-from app.models import User
+from app.models import todos
 
 Base.metadata.create_all(bind=engine)
     
